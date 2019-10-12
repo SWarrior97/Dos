2a0d0e2efbe65cb00c8e36ecc414e748be358f62
SWarrior97 <32170183+SWarrior97@users.noreply.github.com>
2019-10-12 19:34:46 +0100

    first step done
    choose number of players

---
+++ src/Model/Player.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Carlos Gomez
+ */
+public class Player {
+    private String name;
+    private int point;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+    
+    
+    
+}
